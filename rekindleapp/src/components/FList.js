79168e048f14b6a458aa2dba95c056aab520c9e6
JiaqiGao <jiaqigao12@gmail.com>
2019-11-02 11:41:06 -0500

    Bottom nav now

--- rekindleapp/src/components/FList.js
+++ rekindleapp/src/components/FList.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
-import { SafeAreaView, View, Text, Button, Image } from 'react-native';
+import { SafeAreaView, View, Text, Button, StyleSheet, Image } from 'react-native';
 
 export default class FList extends Component {
 
     render() {
       return (
-          <View>
-            <Image
-            style={{width: 50, height: 50}}
-            source={{uri: 'https://facebook.github.io/react-native/img/tiny_logo.png'}}
-            />
-            <Text>{this.props.name}</Text>
+          <View style={{flex: 1, flexDirection: 'row', backgroundColor: 'powderblue'}}>
+            <View style={styles.image}>
+              <Image
+              style={{width: 75, height: 75}}
+              source={{uri: 'https://facebook.github.io/react-native/img/tiny_logo.png'}}
+              />
+            </View>
+
+            <View>
+              <Text>{this.props.name}</Text>
+            </View>
           </View>
-            
-        
       );
     }
   }
+
+  const styles = StyleSheet.create({
+    image: {
+      margin: 2,
+    }
+  });
--- rekindleapp/src/pages/Friends.js
+++ rekindleapp/src/pages/Friends.js
@@ -2,11 +2,51 @@ import React, { Component } from 'react';
 import FList from '../components/FList';  
 import { SafeAreaView, View, Text, Image } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
+import { Icon } from 'react-native-elements'
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons"
+
+
+import BottomNavigation, {
+  FullTab
+} from 'react-native-material-bottom-navigation'
 
 export default class Friends extends Component {
-    static navigationOptions = {
-      title: 'Welcome',
-    };
+  tabs = [
+    {
+      key: 'games',
+      icon: 'gamepad-variant',
+      label: 'Games',
+      barColor: '#388E3C',
+      pressColor: 'rgba(255, 255, 255, 0.16)'
+    },
+    {
+      key: 'movies-tv',
+      icon: 'movie',
+      label: 'Movies & TV',
+      barColor: '#B71C1C',
+      pressColor: 'rgba(255, 255, 255, 0.16)'
+    },
+    {
+      key: 'music',
+      icon: 'music-note',
+      label: 'Music',
+      barColor: '#E64A19',
+      pressColor: 'rgba(255, 255, 255, 0.16)'
+    }
+  ]
+
+  renderIcon = icon => ({ isActive }) => (
+    <Icon size={24} color="white" name={icon} />
+  )
+
+  renderTab = ({ tab, isActive }) => (
+    <FullTab
+      isActive={isActive}
+      key={tab.key}
+      label={tab.label}
+      renderIcon={this.renderIcon(tab.icon)}
+    />
+  )
 
     render() {
       flists = [{
@@ -30,11 +70,16 @@ export default class Friends extends Component {
         <SafeAreaView>
             <ScrollView>
               <Text>Hi friends!</Text>
-              
+
               {flists.map((friend) => {
                 return (<FList key={friend.id} name={friend.name}></FList>
               )})}
             </ScrollView>
+            <BottomNavigation
+            onTabPress={newTab => this.setState({ activeTab: newTab.key })}
+            renderTab={this.renderTab}
+            tabs={this.tabs}
+          />
         </SafeAreaView>
       );
     }
