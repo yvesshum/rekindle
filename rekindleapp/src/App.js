f3700bc78f4f1aad0d6d2f9a6e700c6cff581568
JiaqiGao <jiaqigao12@gmail.com>
2019-11-02 10:07:43 -0500

    Basic navigation

---
+++ rekindleapp/src/App.js
@@ -0,0 +1,116 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import { LoginButton, AccessToken } from 'react-native-fbsdk';
+import HomeScreen from './pages/Home';
+import Friends from './pages/Friends';
+
+
+const MainNavigator = createStackNavigator({
+  Home: {screen: HomeScreen},
+  Friends: {screen: Friends},
+});
+
+const App = createAppContainer(MainNavigator);
+
+export default App;
+// const App: () => React$Node = () => {
+//   return (
+//     <>
+//       <StatusBar barStyle="dark-content" />
+//       <SafeAreaView>
+//         <ScrollView
+//           contentInsetAdjustmentBehavior="automatic"
+//           style={styles.scrollView}>
+         
+//           <View style={styles.body}>
+            
+//                   <LoginButton
+//                   onLoginFinished={
+//                   (error, result) => {
+//                       if (error) {
+//                           console.log("login has error: " + result.error);
+//                       } else if (result.isCancelled) {
+//                           console.log("login is cancelled.");
+//                       } else {
+//                           AccessToken.getCurrentAccessToken().then(
+//                               (data) => {
+//                                   console.log(data.accessToken.toString())
+//                               }
+//                           )
+//                       }
+//                   }
+//               }
+//                   onLogoutFinished={() => console.log("logout.")}/>
+//           </View>
+//         </ScrollView>
+//       </SafeAreaView>
+//     </>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   scrollView: {
+//     backgroundColor: Colors.lighter,
+//   },
+//   engine: {
+//     position: 'absolute',
+//     right: 0,
+//   },
+//   body: {
+//     backgroundColor: Colors.white,
+//   },
+//   sectionContainer: {
+//     marginTop: 32,
+//     paddingHorizontal: 24,
+//   },
+//   sectionTitle: {
+//     fontSize: 24,
+//     fontWeight: '600',
+//     color: Colors.black,
+//   },
+//   sectionDescription: {
+//     marginTop: 8,
+//     fontSize: 18,
+//     fontWeight: '400',
+//     color: Colors.dark,
+//   },
+//   highlight: {
+//     fontWeight: '700',
+//   },
+//   footer: {
+//     color: Colors.dark,
+//     fontSize: 12,
+//     fontWeight: '600',
+//     padding: 4,
+//     paddingRight: 12,
+//     textAlign: 'right',
+//   },
+// });
+
--- rekindleapp/src/pages/Friends.js
+++ rekindleapp/src/pages/Friends.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
 import { SafeAreaView, View, Text, Button } from 'react-native';
 
-export default class HomeScreen extends Component {
+class Friends extends Component {
     static navigationOptions = {
       title: 'Welcome',
     };
     render() {
-      const {navigate} = this.props.navigation;
       return (
         <SafeAreaView>
             <View>
-            <Button onPress={() => navigate('Friends')} title="Friends"/>
-
+                <Text>Hi friends!</Text>
             </View>
         </SafeAreaView>
       );
     }
   }
 
+export default Friends;
