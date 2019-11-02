f9388f941763836fccf94223a47b9a4802e8d301
yvesshum <yvesshum1210@gmail.com>
2019-11-02 14:34:05 -0500

    walkthrough

--- rekindleapp/src/pages/Walkthrough.js
+++ rekindleapp/src/pages/Walkthrough.js
@@ -2,7 +2,7 @@
 
 import React, { Component } from 'react';
 
-import {  View, Image, Text} from 'react-native';
+import {  View, Image, Text, Button, Modal, TouchableOpacity, TouchableHighlight} from 'react-native';
 
 export default class Walkthrough extends Component {
 
@@ -10,22 +10,98 @@ export default class Walkthrough extends Component {
         header: null,
     };
 
+
     constructor(props) {
         super(props);
         this.state={
-
+            pageIndex: 0,
+            modalVisible: false
         };
+        this.setModalVisible = this.setModalVisible.bind(this)
 
+    }
+    setModalVisible(visible) {
+        this.setState({modalVisible: visible});
+    }
 
+    increment() {
+        let prev = this.state.pageIndex;
+        if (prev < 2){
+            this.setState({pageIndex: prev + 1})
+        }
+
+    }
+    decrement() {
+        let prev = this.state.pageIndex;
+        if (prev > 0){
+            this.setState({pageIndex: prev - 1})
+        }
+
+    }
+
+    prevIsDisabled() {
+        if (this.state.pageIndex == 0) return true;
     }
 
 
     render() {
+
+        let page;
+
+        if (this.state.pageIndex === 0) {
+            page = <Image source={require('../../assets/walkthrough/w1.jpg')} style={{width: 400, height:700, marginTop: 40, marginLeft: 5}}/>
+        }
+        else if (this.state.pageIndex === 1) {
+            page = <Image source ={require('../../assets/walkthrough/w2.jpg')} style={{width: 400, height:700, marginTop: 40, marginLeft: 5}}/>;
+        }
+        else {
+            page =<Image source ={require('../../assets/walkthrough/w3.jpg')}  style={{width: 400, height:700, marginTop: 40, marginLeft: 5}}/>;
+        }
+
+        let navbutton;
+
+        if (this.state.pageIndex !== 2) {
+            navbutton =
+                <View style={{flexDirection: 'row', width:'100%', justifyContent:'space-evenly'}}>
+                    <Button title={'previous'} onPress={()=>this.decrement()} disabled={this.prevIsDisabled()}/>
+                    <Button title={'next'} onPress={()=>this.increment()}/>
+                </View>
+        }
+        else {
+            navbutton =
+                <View style={{backgroundColor: '#925dc5', borderRadius: 30, width:'70%', height: 50, justifyContent:'center', marginTop: 20}}>
+                    <Button title={'START CATCHING UP!'} color={'#FFFFFF'} onPress={() => this.setModalVisible(true)} />
+                </View>
+        }
         return (
-            <View style={{flex: 1, borderBottomWidth: 1}}>
-                <Image source={require('../../assets/walkthrough/w1.jpg')} style={{width: 400, height:700, position:'absolute', marginTop: 40, marginLeft: 5}}/>
-            
-            </View>
+
+                <View style={{flex: 1, flexDirection: 'column', alignItems: 'center'}}>
+                    <Modal
+                        animationType="slide"
+                        transparent={false}
+                        visible={this.state.modalVisible}
+                        onRequestClose={() => {
+                            Alert.alert('Modal has been closed.');
+                        }}>
+                        <View style={{marginTop: 22}}>
+                            <View>
+                                <Text>Hello World!</Text>
+
+                                <TouchableHighlight
+                                    onPress={() => {
+                                        this.setModalVisible(!this.state.modalVisible);
+                                    }}>
+                                    <Text>Hide Modal</Text>
+                                </TouchableHighlight>
+                            </View>
+                        </View>
+                    </Modal>
+                    {page}
+                    {navbutton}
+
+                </View>
+
+
         );
     }
 }
