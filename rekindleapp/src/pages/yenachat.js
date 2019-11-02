fefc2e339b8f2e98944739c88d32594dd1fc76d7
yvesshum <yvesshum1210@gmail.com>
2019-11-02 18:05:26 -0500

    popup alert

--- rekindleapp/src/pages/yenachat.js
+++ rekindleapp/src/pages/yenachat.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 import FList from '../components/FList';
-import { SafeAreaView, View, Text, Image, StyleSheet } from 'react-native';
+import {
+    SafeAreaView,
+    View,
+    Text,
+    Image,
+    StyleSheet,
+    TouchableOpacity,
+    Button
+} from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { Icon } from 'react-native-elements'
 import { SearchBar } from 'react-native-elements';
+import Modal from 'react-native-modal'
 
 
 import BottomNavigation, {
@@ -11,16 +20,53 @@ import BottomNavigation, {
 } from 'react-native-material-bottom-navigation'
 
 export default class YenaChat extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            modalVisible: false,
+        };
+        this.setModalVisible = this.setModalVisible.bind(this);
+    }
 
     static navigationOptions = {
         header: null,
     };
 
+    setModalVisible(visible) {
+        this.setState({modalVisible: visible});
+    }
+
+    handleButtonPress() {
+        this.setModalVisible(true);
+    }
+
+    continueToMain() {
+        this.setModalVisible(false);
+
+    }
+
     render() {
+
+        let modalContent =
+            <View style={{  backgroundColor: '#FFFFFF', height: 236, width:370, alignSelf: 'center', borderRadius: 20, alignItems: 'center'}}>
+                <Text style={{fontWeight: 'bold', marginTop: 20, fontSize: 17}}>Notice!</Text>
+                <Text style={{textAlign: 'center', marginTop: 5, lineHeight: 20,}}>Yena will only receive this if she also sends you something!</Text>
+                <View style={{backgroundColor: '#925dc5', borderRadius:40, height:57, width:278,  justifyContent:'center', marginTop: 30}}>
+                    <Button color='#FFFFFF' title={'SEND'} onPress={() => this.continueToMain()}/>
+                </View>
+
+                <Button color='#925dc5' title={'cancel'} onPress={() => this.setModalVisible(false)}/>
+            </View>
+
 
         return (
             <View>
-                <Image source={require('../../assets/yenachat.jpg')} style={{width:410, height: 780, marginLeft: 0, marginTop: 70}}/>
+                <TouchableOpacity onPress={() => this.handleButtonPress()}>
+                    <Image source={require('../../assets/yenachat.jpg')} style={{width:410, height: 780, marginLeft: 0, marginTop: 70, position:'absolute'}}/>
+                </TouchableOpacity>
+                <Modal isVisible={this.state.modalVisible}>
+                    {modalContent}
+                </Modal>
             </View>
 
 
